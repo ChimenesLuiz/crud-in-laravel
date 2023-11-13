85635ff34ceb19c006d70a9553a98711abbddfb1
= <=>
2023-11-13 10:42:08 -0400

    asd
    asd

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,22 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']) -> name('home.index');
+
+
+//LOGIN
+Route::get('/', [UserController::class, 'login']) -> name('user.login');
+
+//HOME
+Route::controller(HomeController::class) -> group(function () {
+    Route::get('/home', 'index') -> name('home.index');
+});
 
 //USER
-Route::get('/user', [UserController::class, 'index']) -> name('user.index');
-Route::get('/user/create', [UserController::class, 'create']) -> name('user.create');
-Route::get('/user/create/store', [UserController::class, 'store']) -> name('user.store');
-Route::get('/user/edit/{id}', [UserController::class, 'edit']) -> name('user.edit');
-Route::get('/user/update/{id}', [UserController::class, 'update']) -> name('user.update');
-Route::get('/user/destroy/{id}', [UserController::class, 'destroy']) -> name('user.destroy');
+Route::controller(UserController::class) -> group(function () {
+    Route::get('/user', 'index') -> name('user.index');
+    Route::get('/user/create', 'create') -> name('user.create');
+    Route::get('/user/create/store', 'store') -> name('user.store');
+    Route::get('/user/edit/{id}', 'edit') -> name('user.edit');
+    Route::get('/user/update/{id}', 'update') -> name('user.update');
+    Route::get('/user/destroy/{id}', 'destroy') -> name('user.destroy');
+});
